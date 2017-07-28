4d27dee41b7bbc047194ba479d1cf84beffda61e
qiaoguang <qiaozeguang@163.com>
2017-07-28 16:51:34 +0800

    继承DispatcherServlet，添加服务注册功能

---
+++ WeServicePlat/WeService/src/main/java/com/jumore/we/service/server/web/WeServiceDispatcherServlet.java
@@ -0,0 +1,24 @@
+/** 
+ * Project Name:WeService 
+ * File Name:WeServiceDispatcherServlet.java 
+ * Package Name:com.jumore.we.service.server.register
+ * Copyright (c) 2017, JUMORE Co.,Ltd. All Rights Reserved. 
+ *
+ * @author 乔广
+ * @date 2017年7月28日 下午4:44:18
+ */
+package com.jumore.we.service.server.web;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * 添加服务注册功能
+ * 
+ * @author 乔广
+ * @date 2017年7月28日 下午4:44:18
+ * @version 
+ * @see
+ */
+public class WeServiceDispatcherServlet extends DispatcherServlet {
+
+}
