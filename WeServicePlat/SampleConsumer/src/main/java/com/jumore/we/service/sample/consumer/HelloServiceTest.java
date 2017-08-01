b4f0fb5d7ae6e6c3aad417fc515a25ac12630e43
qiaoguang <qiaozeguang@163.com>
2017-08-01 16:54:52 +0800

    Demo

--- WeServicePlat/SampleConsumer/src/main/java/com/jumore/we/service/sample/consumer/HelloServiceTest.java
+++ WeServicePlat/SampleConsumer/src/main/java/com/jumore/we/service/sample/consumer/HelloServiceTest.java
@@ -1,21 +1,7 @@
 package com.jumore.we.service.sample.consumer;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.net.URI;
-
-import org.I0Itec.zkclient.ZkClient;
-
-import com.jumore.we.service.client.ServiceIntercept;
 import com.jumore.we.service.client.ServiceProxy;
-import com.jumore.we.service.client.mapping.ZookeeperServiceMapping;
-import com.jumore.we.service.client.mapping.selector.RandomServiceSelector;
-import com.jumore.we.service.client.mapping.selector.ServiceSelector;
-import com.jumore.we.service.client.mapping.selector.WeightRandomServiceSelector;
 import com.jumore.we.service.sample.api.HelloService;
-import com.thoughtworks.paranamer.AnnotationParanamer;
-import com.thoughtworks.paranamer.BytecodeReadingParanamer;
-import com.thoughtworks.paranamer.CachingParanamer;
 
 public class HelloServiceTest {
 
