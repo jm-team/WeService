3558f19026b9b1c8c2897ffc50d5c70975d808df
qiaoguang <qiaozeguang@163.com>
2017-08-08 15:14:27 +0800

    pojo converter

---
+++ WeServicePlat/WeService/src/main/java/com/jumore/we/service/client/response/converter/PojoResultConverter.java
@@ -0,0 +1,37 @@
+/** 
+ * Project Name:WeService 
+ * File Name:PojoResultConverter.java 
+ * Package Name:com.jumore.we.service.client.response.converter
+ * Copyright (c) 2017, JUMORE Co.,Ltd. All Rights Reserved. 
+ *
+ * @author 乔广
+ * @date 2017年8月8日 下午1:58:27
+ */
+package com.jumore.we.service.client.response.converter;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Function: 基础类型，string，date，array和pojo的转换
+ * 
+ * @author 乔广
+ * @date 2017年8月8日 下午1:58:27
+ * @version 
+ * @see
+ */
+public class PojoResultConverter implements ResponseResultConverter {
+
+    @Override
+    public boolean support(Class<?> clazz) {
+        return Object.class.equals(clazz);
+    }
+
+    /** 
+     * 简单的基于fastjson的转换.
+     */
+    @Override
+    public Object convert(String data, Class<?> clazz) {
+        return JSONObject.parseObject(data, clazz);
+    }
+
+}
